2171a5516f3d9e11df571771acf3742bbb747260
juliocesar <juliocesaar.ma@gmail.com>
2021-05-08 11:36:04 -0300

    initial commit

---
+++ unitPrincipal.pas
@@ -0,0 +1,45 @@
+unit unitPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    title: TLabel;
+    Label1: TLabel;
+    bookTitle: TEdit;
+    Label2: TLabel;
+    authorName: TEdit;
+    Label3: TLabel;
+    Label4: TLabel;
+    genreSelect: TComboBox;
+    inStock: TCheckBox;
+    Label5: TLabel;
+    RadioButton1: TRadioButton;
+    RadioButton2: TRadioButton;
+    RadioButton3: TRadioButton;
+    Memo1: TMemo;
+    Label6: TLabel;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    CheckBox3: TCheckBox;
+    btnRegister: TButton;
+    btnDelete: TButton;
+    btnSearch: TButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
